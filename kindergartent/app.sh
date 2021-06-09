0006eb7badf363bc63cdd8d094ecdd00118a532e
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2021-06-09 16:12:21 +0300

    fixed some typos

--- kindergartent/app.sh
+++ kindergartent/app.sh
@@ -4,7 +4,7 @@ migrate=$(python manage.py migrate | grep "No migrations to apply")
 if [ -z "$migrate" ]
 then
 	python manage.py makemigrations
-	python␣manage.py␣migrate
+	python manage.py migrate
 fi
 
 gunicorn kindergarten.wsgi:application --bind 0.0.0.0:8000
